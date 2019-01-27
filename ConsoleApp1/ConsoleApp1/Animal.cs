f7e37a6623be11207c911bc65ea0ec15097b9270
aamir <wasi3aamir@gmail.com>
2019-01-27 23:40:03 +0600

    practice perfactly

---
+++ ConsoleApp1/ConsoleApp1/Animal.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Animal
+    {
+        private string name;
+        protected string sound;
+
+        protected AnimalIDInfo animalInfo = new AnimalIDInfo();
+        public void SetAnimalIDInfo(int idNum,string owner)
+        {
+            animalInfo.IdNum = idNum;
+            animalInfo.Owner = owner;
+        }
+        public void GetAnimalInfo()
+        {
+            Console.WriteLine($"{Name} had the Id of {animalInfo.IdNum} & is owned by {animalInfo.Owner}");
+        }
+
+
+        public virtual void MakeSound()
+        {
+            Console.WriteLine($"{Name} says {Sound}");
+        }
+
+        public Animal()
+            :this("No Name","No Sound") { }
+        public Animal(string name)
+            :this(name,"No Sound") { }
+
+        public Animal(string name,string sound)
+        {
+            Name = name;
+            Sound = sound;
+        }
+
+        
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (!value.Any(char.IsDigit))
+                {
+                    name = value;
+                }
+                else
+                {
+                    name = "No Name";
+                }
+            }
+        }
+
+        public string Sound
+        {
+            get { return sound; }
+            set
+            {
+                if (value.Length >10)
+                {
+                    sound = "No Sound";
+                }
+                else
+                {
+                    sound = value;
+                }
+            }
+        }
+        //inner class
+        public class AnimalHealth
+        {
+            public bool HealthyWeight(double height,double weight)
+            {
+                double calc = height / weight;
+                if ((calc >= .18) && (calc <= .27))
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal whiskers = new Animal()
+            {
+                Name = "Whiskers",
+                Sound = "Meow"
+            };
+
+            Dog grover = new Dog()
+            {
+                Name = "Grover",
+                Sound = "Woof",
+                Sound2 = "Greee"
+            };
+            whiskers.MakeSound();
+            grover.MakeSound();
+            whiskers.Sound = "Meoooow";
+            grover.Sound = "Grrreeee";
+            grover.Name = "gronver";
+            grover.MakeSound();
+            whiskers.MakeSound();
+
+
+            whiskers.SetAnimalIDInfo(123456, "Sally Smith");
+            grover.SetAnimalIDInfo(156, "Paul Brown");
+
+
+            whiskers.GetAnimalInfo();
+            grover.GetAnimalInfo();
+
+
+            //innerClass
+            Animal.AnimalHealth GetHealth = new Animal.AnimalHealth();
+            Console.WriteLine("Is my Animal is healthy : {0}",GetHealth.HealthyWeight(11,46));
+
+
+            //polymorphsm
+
+            Animal monkey = new Animal()
+            {
+                Name = "Happy",
+                Sound = "Eeee"
+            };
+            Animal spot = new Dog()
+            {
+                Name = "Spot",
+                Sound = "Gooooowww",
+                Sound2 = "Woooof"
+            };
+            monkey.MakeSound();
+            spot.MakeSound();
+        }
+    }
+}
